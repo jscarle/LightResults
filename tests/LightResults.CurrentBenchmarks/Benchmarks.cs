ebfbfe0cd283db6366e824672c4959d021b6932e
Jean-Sebastien Carle <29762210+jscarle@users.noreply.github.com>
2024-02-18 09:09:04 -0500

    Added benchmarks for the Errors indexer.

--- tests/LightResults.CurrentBenchmarks/Benchmarks.cs
+++ tests/LightResults.CurrentBenchmarks/Benchmarks.cs
@@ -21,6 +21,14 @@ public class Benchmarks
     }
 
     [Benchmark]
+    public void Current_ResultBaseIndexer()
+    {
+        for (var iteration = 0; iteration < Iterations; iteration++)
+            _ = FailedResult.Errors[0];
+    }
+
+    /*
+    [Benchmark]
     public void Current_ResultOk()
     {
         for (var iteration = 0; iteration < Iterations; iteration++)
@@ -97,4 +105,5 @@ public class Benchmarks
         for (var iteration = 0; iteration < Iterations; iteration++)
             _ = new Error(ErrorMessage);
     }
+*/
 }
