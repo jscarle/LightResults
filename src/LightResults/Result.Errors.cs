19c4ed60fe9167cf11c865464efeb18229cbdd88
Chris Kelly <cjkelly165@gmail.com>
2024-06-26 12:15:53 +1000

    Add `HasError<T>(out T error)` method (#31)

--- src/LightResults/Result.Errors.cs
+++ src/LightResults/Result.Errors.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace LightResults;
 
 partial struct Result
@@ -36,5 +38,41 @@ partial struct Result
             }
 
         return typeof(TError) == typeof(Error);
+    }
+
+    /// <inheritdoc/>
+    public bool HasError<TError>([MaybeNullWhen(false)] out TError error)
+        where TError : IError
+    {
+        if (_isSuccess)
+        {
+            error = default;
+            return false;
+        }
+
+        if (_errors.HasValue)
+        {
+            // Do not convert to LINQ, this creates unnecessary heap allocations.
+            // For is the most efficient way to loop. It is the fastest and does not allocate.
+            // ReSharper disable once ForCanBeConvertedToForeach
+            // ReSharper disable once LoopCanBeConvertedToQuery
+            for (var index = 0; index < _errors.Value.Length; index++)
+            {
+                if (_errors.Value[index] is not TError)
+                    continue;
+
+                error = (TError)_errors.Value[index];
+                return true;
+            }
+        }
+
+        if (typeof(TError) == typeof(Error))
+        {
+            error = (TError)Error.Empty;
+            return true;
+        }
+
+        error = default;
+        return false;
     }
 }
